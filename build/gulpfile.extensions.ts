02deb29e5db8f800bb7a01d9c912df565005a4d4
Mahfoodh Al Jufaili <alra7il@users.noreply.github.com>
2025-12-02 07:55:17 -0800

    Embdedded spectrum-ai as a built-in extension

--- build/gulpfile.extensions.ts
+++ build/gulpfile.extensions.ts
@@ -72,6 +72,9 @@ const compilations = [
 	'extensions/vscode-colorize-perf-tests/tsconfig.json',
 	'extensions/vscode-test-resolver/tsconfig.json',
 
+	// Add spectrum-ai extension as a built-in extension
+	'extensions/spectrum-ai/tsconfig.json',
+
 	'.vscode/extensions/vscode-selfhost-test-provider/tsconfig.json',
 	'.vscode/extensions/vscode-selfhost-import-aid/tsconfig.json',
 ];
